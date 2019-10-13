1e67f94ae4695fd22a20079d5605b926ad984921
the-redback <maruf.2hin@gmail.com>
2019-10-13 12:51:36 +0600

    skeleton with makefile and dockerfile

---
+++ Makefile
@@ -0,0 +1,20 @@
+IMAGE_PREFIX = maruftuhin
+IMAGE_REPO = conways
+IMAGE_VERSION ?= latest
+IMAGE_NAME = $(IMAGE_PREFIX)/$(IMAGE_REPO):$(IMAGE_VERSION)
+
+.PHONY: build push run kube-run
+
+build:
+	docker build --no-cache -t $(IMAGE_NAME) .
+
+push:
+	docker push $(IMAGE_NAME)
+
+run:
+	docker run -p 12345:12345 -ti --rm $(IMAGE_NAME)
+
+kube-run:
+	kubectl run -i -t --image=$(IMAGE_NAME) shell --restart=Never --rm
+
+default: build
---
+++ src/conways.go
@@ -0,0 +1,29 @@
+package conways
+
+import (
+	"fmt"
+	"github.com/bmizerany/pat"
+	"io"
+	"log"
+	"net/http"
+)
+
+// hello world, the web server
+func HelloServer(w http.ResponseWriter, req *http.Request) {
+	io.WriteString(w, "hello, "+req.URL.Query().Get(":name")+"!\n")
+}
+
+func ListenAndServe() {
+	fmt.Println("start serving...")
+
+	m := pat.New()
+	m.Get("/hello/:name", http.HandlerFunc(HelloServer))
+
+	// Register this pat with the default serve mux so that other packages
+	// may also be exported. (i.e. /debug/pprof/*)
+	http.Handle("/", m)
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
